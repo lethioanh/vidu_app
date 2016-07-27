4c6aba64ef0e2345c748b99e93c6fea2c1446241
at-oanhlt <Oanhlt@asiantech.vn>
2016-07-27 11:52:00 +0700

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,18 +8,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home |  #{@base_title}" # chấp nhận title "..." xuất hiện trong page home
+    assert_select "title", "Home | #{@base_title}" # chấp nhận title "..." xuất hiện trong page home
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help |  #{@base_title}" # chấp nhận title "..." xuất hiện trong page help
+    assert_select "title", "Help | #{@base_title}" # chấp nhận title "..." xuất hiện trong page help
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About |  #{@base_title}" # chấp nhận title "..." xuất hiện trong page about
+    assert_select "title", "About | #{@base_title}" # chấp nhận title "..." xuất hiện trong page about
   end
 end
