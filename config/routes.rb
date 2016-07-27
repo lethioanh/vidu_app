382d9beea095c64a2cc2709ba21f9abcbb93e0cf
at-oanhlt <Oanhlt@asiantech.vn>
2016-07-27 14:31:28 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
 
 
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root "application#hello"
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  # get 'static_pages/home' chuyen thanh
+  get '/home', to: 'static_pages#home'
+
+  # get 'static_pages/help' chuyen thanh
+  get '/help', to: 'static_pages#help'
+
+  # get 'static_pages/about' chuyen thanh
+  get '/about', to: 'static_pages#about'
+
+  # get 'static_pages/contact' chuyen thanh
+  get '/contact', to: 'static_pages#contact'
+
+  # get new users = sign up of users
+  get '/signup', to: 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,19 +6,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    # get static_pages_home_ur chuyen thanh
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}" # chấp nhận title "..." xuất hiện trong page home
   end
 
   test "should get help" do
-    get static_pages_help_url
+    # get static_pages_help_url chuyen thanh
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}" # chấp nhận title "..." xuất hiện trong page help
   end
 
   test "should get about" do
-    get static_pages_about_url
+    # get static_pages_about_url chuyen thanh
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}" # chấp nhận title "..." xuất hiện trong page about
   end
