f16a32d0bda85d47e2cd6615664bdb8d460362bc
at-oanhlt <Oanhlt@asiantech.vn>
2016-07-29 11:05:51 +0700

    remember_token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # # Returns the current logged-in user (if any)
@@ -19,10 +19,10 @@ module SessionsHelper
   #returns the user corresponding to the remember token cookie
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by_id(user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       # raise # the test still pass, so this branch is currently untested
-      user = User.find_by_id(user_id)
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
